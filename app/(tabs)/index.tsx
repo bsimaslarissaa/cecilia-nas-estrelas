92e1b25c620ec64c0a55978eb0bbe1dc6c9f871d
mavi <mavisberrye@gmail.com>
2026-07-02 15:29:09 -0300

    feat: adição de responsividade

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,60 +2,57 @@ import { Image } from 'expo-image';
 import React from 'react';
 import { ImageBackground, Pressable, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from "react-native-safe-area-context";
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export default function HomeScreen() {
   return (
     <ImageBackground
-  source={require('@/assets/images/fundocecilia.jpeg')}
-  style={styles.background}
-  resizeMode="cover"
->
-  <SafeAreaView style={styles.containerContent}>
+      source={require('@/assets/images/fundocecilia.jpeg')}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <SafeAreaView style={styles.containerContent}>
 
-    {/* Logo */}
-    <Image
-      source={require('@/assets/images/logo-cecilia.png')}
-      style={styles.logo}
-      contentFit="contain"
-    />
+        <View style={styles.topoContainer}>
+          <Image
+            source={require('@/assets/images/logo-cecilia.png')}
+            style={styles.logo}
+            contentFit="contain"
+          />
+          <Text style={styles.subtitulo}>
+            Aprenda astronomia{"\n"}brincando ✨
+          </Text>
+        </View>
 
-    {/* Subtítulo */}
-    <Text style={styles.subtitulo}>
-      Aprenda astronomia{"\n"}brincando ✨
-    </Text>
+        <Image
+          source={require('@/assets/images/ceciliainicio.png')}
+          style={styles.cecilia}
+          contentFit="contain"
+        />
 
-    {/* Cecília */}
-    <Image
-      source={require('@/assets/images/ceciliainicio.png')}
-      style={styles.cecilia}
-      contentFit="contain"
-    />
+        <View style={styles.baseContainer}>
+          <Pressable style={styles.botao}>
+            <Text style={styles.textoBotao}>
+              Entrar como convidado
+            </Text>
+          </Pressable>
 
-    {/* Botão */}
-    <Pressable style={styles.botao}>
-      <Text style={styles.textoBotao}>
-        Entrar como convidado
-      </Text>
-    </Pressable>
+          <View style={styles.loginContainer}>
+            <Text style={styles.loginTexto}>
+              Já possui uma conta?
+            </Text>
+            <Text style={styles.loginLink}>
+              Entrar
+            </Text>
+          </View>
+        </View>
 
-    {/* Login */}
-    <View style={styles.loginContainer}>
-      <Text style={styles.loginTexto}>
-        Já possui uma conta?
-      </Text>
-
-      <Text style={styles.loginLink}>
-        Entrar
-      </Text>
-    </View>
-
-  </SafeAreaView>
-</ImageBackground>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-
   background: {
     flex: 1,
   },
@@ -63,72 +60,77 @@ const styles = StyleSheet.create({
   containerContent: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-evenly',
-    paddingHorizontal: 20,
-    paddingTop: 40,
-    paddingBottom: 30,
+    justifyContent: 'space-between', 
+    paddingHorizontal: wp('5%'),    
+    paddingVertical: hp('2%'),
   },
 
- logo: {
-  width: '120%',
-  height: 280,
-  marginTop: 20,
-  marginBottom: -70,
-},
+  topoContainer: {
+    marginTop: -hp('10%'),
+    alignItems: 'center',
+    width: '100%',
+    gap: hp('0%'),
+  },
 
- subtitulo: {
-  color: '#FFFFFF',
-  fontSize: 18,
-  textAlign: 'center',
-  marginTop: -30,
-  marginBottom: -20,
-  lineHeight: 26,
-  fontWeight: '500',
-},
+  logo: {
+    width: wp('130%'), 
+    height: hp('36%'), 
+  },
+
+  subtitulo: {
+    color: '#FFFFFF',
+    fontSize: hp('2.2%'),           
+    textAlign: 'center',
+    lineHeight: hp('3%'),           
+    fontWeight: '500',
+    marginTop: -hp('10%'), 
+  },
 
   cecilia: {
-  width: '90%',
-  height: 320,
-},
+    width: wp('100%'),               
+    height: hp('38%'),
+  },
+
+
+  baseContainer: {
+    alignItems: 'center',
+    width: '100%',
+    gap: hp('2.5%'),
+  },
 
   botao: {
-    width: '90%',
+    width: wp('85%'),
     backgroundColor: '#5C38D6',
-    paddingVertical: 18,
-    borderRadius: 35,
+    paddingVertical: hp('2%'),      
+    borderRadius: wp('8%'),         
     alignItems: 'center',
-
     shadowColor: '#8A6CFF',
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
+    shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.35,
     shadowRadius: 8,
     elevation: 8,
   },
+
   textoBotao: {
     color: '#FFFFFF',
-    fontSize: 21,
+    fontSize: hp('2.4%'),           
     fontWeight: 'bold',
   },
 
   loginContainer: {
     alignItems: 'center',
-    marginTop: -10,
   },
 
   loginTexto: {
     color: '#FFFFFF',
-    fontSize: 17,
+    fontSize: hp('2%'),
   },
 
   loginLink: {
     color: '#B78DFF',
-    fontSize: 22,
+    fontSize: hp('2.5%'),           
     fontWeight: 'bold',
-    marginTop: 6,
+    marginTop: hp('0.5%'),
     textDecorationLine: 'underline',
   },
-
 });
