6f96c7fd05f1d18b86789bd20d8f218d1104a8ad
mavi <mavisberrye@gmail.com>
2026-07-02 14:38:06 -0300

    puxando alterações

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { ImageBackground, StyleSheet, Text, View, Pressable, Alert  } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Pressable, Alert, SafeAreaView } from 'react-native';
 import { Image } from 'expo-image';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export default function HomeScreen() {
   return (
@@ -9,6 +10,7 @@ export default function HomeScreen() {
       style={styles.background}
       resizeMode="cover"
     >
+       <SafeAreaView style={styles.safeArea}>
       <View style={styles.containerContent}>
         <Text style={styles.texto}>Cecília nas estrelas </Text>
 
@@ -20,6 +22,7 @@ export default function HomeScreen() {
           <Text style={styles.textoBotao}> Entrar como convidado</Text>
         </Pressable>
       </View>
+      </SafeAreaView>
     </ImageBackground>
   );
 }
@@ -28,43 +31,44 @@ const styles = StyleSheet.create({
   background: {
     flex: 1, 
   },
+  safeArea: {
+    flex: 1,
+    height: '100%', // Força o container seguro a usar toda a altura disponível
+  },
   containerContent: {
     flex: 1,
-    justifyContent: 'flex-start',
-    paddingTop: 100,
+    height: '100%',          // Garante que a View interna se espalhe pela tela toda
+    justifyContent: 'center', // Agora o 'center' vai funcionar perfeitamente
     alignItems: 'center',
-    gap: 90
-   
+    gap: hp('6%'),  
   },
   texto: {
     color: '#fff',
-    fontSize: 35,
+    fontSize: hp('4%'),
     fontWeight: 'bold',
     textAlign: 'center',
     fontFamily: 'serif',
-    
   },
   cecilia: {
-    width: 300,  
-    height: 300, 
-    borderRadius: 50,
+    width: wp('65%'),  
+    height: wp('65%'),     
+    borderRadius: wp('8%'),
   },
-   botao: {
+  botao: {
     backgroundColor: '#1e1289',
-    paddingVertical: 12,
-    paddingHorizontal: 28,
-    borderRadius: 25, 
-    marginTop: 10, 
+    paddingVertical: hp('1.8%'), 
+    paddingHorizontal: wp('8%'),  
+    borderRadius: wp('6%'), 
+    marginTop: hp('1%'), 
     elevation: 3, 
     shadowColor: '#000', 
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
   },
-
   textoBotao: {
     color: '#f7f7f7',
-    fontSize: 16,
+    fontSize: hp('2%'),
     fontWeight: 'bold',
   },
 });
